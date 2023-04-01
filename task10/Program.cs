7f68cf68d1a160ff1891b4990b2dc0b020c3209b
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-04-01 22:12:56 +0200

    task 10 added

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+
+string n = Console.ReadLine();
+
+int i = n.Length;
+
+if (i<3) Console.WriteLine("Третьей цифры нет");
+else {
+        Console.WriteLine(n[2]); //Console.WriteLine(n.Substring(2, 1));
+}
